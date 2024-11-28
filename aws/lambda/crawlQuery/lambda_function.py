27eea05c910c39a30ab586aaf8a5da98530c6402
bbungsang94 <bbubgsang94@ajou.ac.kr>
2024-11-28 21:15:14 +0900

    fixed human error

--- aws/lambda/crawlQuery/lambda_function.py
+++ aws/lambda/crawlQuery/lambda_function.py
@@ -1,5 +1,6 @@
 import logging
 import os
+
 from spider.query import GachiGaHandler
 from spider.structure import Node
 from spider.utils.logging import init_logging
@@ -14,14 +15,13 @@ def lambda_handler(event, context):
     for event_key, key in kw_map.items():
         kwargs[key] = event.get(event_key)
 
-    init_logging(logging.DEBUG, "query-to-rdb-using-lambda.log", dir_path='/tmp/')
+    init_logging(logging.INFO, "query-to-rdb-using-lambda.log", dir_path='/tmp/')
     handler_kwargs = {
         "host": os.getenv("host"),
         "user": os.getenv("user"),
         "password": os.getenv("password"),
-        "database": os.getenv("database")
+        "database": os.getenv("database"),
     }
-
     try:
         handler = GachiGaHandler(**handler_kwargs)
         for node_raw in kwargs['nodes']:
@@ -30,4 +30,17 @@ def lambda_handler(event, context):
         return {'statusCode': 200, 'message': "Succeeded"}
     
     except Exception as e:
+        pass
         return {'statusCode': -1, 'message': "Unexpected exit"}
+
+def make_dummy_nodes(db_ip, db_port=None):
+    from pymongo import MongoClient
+    from spider.utils.mongo import get_data_with
+    
+    client = MongoClient(host=db_ip, port=db_port)
+    server_info = client.server_info()
+    collection = client['Pages']
+    collection = collection['Nodes']
+    
+    alternatives = get_data_with(collection, label='Transformed')
+    return [x.to_dict() for x in alternatives]
