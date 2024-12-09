4aad21ebbf0e66e06f9a0090d70d427986049c72
bbungsang94 <bbubgsang94@ajou.ac.kr>
2024-12-09 20:19:32 +0900

    passed check hostname condition

--- spider/manager/state/extract.py
+++ spider/manager/state/extract.py
@@ -14,12 +14,7 @@ class Extract(State):
             self.logger.info("Extract fan out links from fetched html")
             base_url = get_base_url(self.node.url)
             for i, node in reversed(list(enumerate(self.node.fan_out))):
-                parsed_url = urlparse(node.url)
-                if parsed_url.hostname == None:
-                    self.node.fan_out[i].url = urljoin(base_url, node.url)
-                else:
-                    del self.node.fan_out[i]
-                    
+                self.node.fan_out[i].url = urljoin(base_url, node.url)             
             self.parent.transit(Unwrap(node=self.node, parent=self.parent))
         except Exception as e:
             self.node.label = "Failed Attach Base URL"
